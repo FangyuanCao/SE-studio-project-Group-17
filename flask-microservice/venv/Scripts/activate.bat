d9ec1c5933f813c83a1104fdc5f70f5f2ebbdd59
FangyuanCao <55753314+FangyuanCao@users.noreply.github.com>
2025-01-27 12:17:31 -0500

    Now you can use postman to check API
    Now you can use postman to check API

--- flask-microservice/venv/Scripts/activate.bat
+++ flask-microservice/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\React Project\micro service\venv
+set VIRTUAL_ENV=E:\React Project\SE-studio-project-Group-17\flask-microservice\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
